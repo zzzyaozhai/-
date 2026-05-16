98e6ac8edb70af09a5e3221f4aa55c3b81e70aa1
zzzyaozhai <2128304224@qq.com>
2026-05-17 01:11:23 +0800

    feat: Update build process and dependencies
    Refactors the build process by removing unnecessary file copying and updates dependencies and devDependencies to newer versions. Also, updates the Node.js version in the CI workflow for building the APK.

--- vite.config.ts
+++ vite.config.ts
@@ -14,7 +14,7 @@ export default defineConfig(({mode}) => {
     },
     resolve: {
       alias: {
-        '@': path.resolve(__dirname, '.'),
+        '@': path.resolve('.'),
       },
     },
     server: {
